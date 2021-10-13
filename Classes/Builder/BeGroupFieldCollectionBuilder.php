69a4586e4f675022d2e36f04ddfeaf5e0470ac00
Sebastian Hofer <sebastian.hofer@pluswerk.ag>
2021-10-13 08:21:16 +0200

    ✨ Make field collection buildable from be_groups database values
    Related: BEPERM-5

---
+++ Classes/Builder/BeGroupFieldCollectionBuilder.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pluswerk\BePermissions\Builder;
+
+use Pluswerk\BePermissions\Collection\BeGroupFieldCollection;
+use Pluswerk\BePermissions\Configuration\ExtensionConfiguration;
+use Pluswerk\BePermissions\Configuration\NoValueObjectConfiguredException;
+use Pluswerk\BePermissions\Value\BeGroupFieldInterface;
+
+final class BeGroupFieldCollectionBuilder
+{
+    private ExtensionConfiguration $config;
+
+    /**
+     * BeGroupFieldCollectionBuilder constructor.
+     * @param ExtensionConfiguration $config
+     */
+    public function __construct(ExtensionConfiguration $config)
+    {
+        $this->config = $config;
+    }
+
+    public function buildFromDatabaseValues(array $dbValues): BeGroupFieldCollection
+    {
+        $collection = new BeGroupFieldCollection();
+
+        foreach ($dbValues as $dbFieldName => $dbValue) {
+
+            try {
+                $valueClass = $this->config->getClassNameByFieldName($dbFieldName);
+            } catch (NoValueObjectConfiguredException $exception) {
+                continue;
+            }
+
+            // @todo: Move to a BeGroupFieldFactory
+            $implementArray = class_implements($valueClass);
+            if (in_array(BeGroupFieldInterface::class, $implementArray)) {
+                $valueObject = $valueClass::createFromDBValue($dbValue);
+                $collection->add($valueObject);
+            }
+        }
+
+        return $collection;
+    }
+}
---
+++ Tests/Unit/Builder/BeGroupFieldCollectionBuilderTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pluswerk\BePermissions\Tests\Unit\Builder;
+
+use Pluswerk\BePermissions\Collection\BeGroupFieldCollection;
+use Pluswerk\BePermissions\Configuration\ExtensionConfiguration;
+use Pluswerk\BePermissions\Value\NonExcludeFields;
+use Pluswerk\BePermissions\Value\TablesSelect;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use Pluswerk\BePermissions\Builder\BeGroupFieldCollectionBuilder;
+
+/**
+ * @covers \Pluswerk\BePermissions\Builder\BeGroupFieldCollectionBuilder
+ */
+final class BeGroupFieldCollectionBuilderTest extends UnitTestCase
+{
+    protected function setUp(): void
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['be_permissions'] = [
+            'valueObjectMapping' => [
+                'non_exclude_fields' => NonExcludeFields::class,
+                'tables_select' => TablesSelect::class
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function a_be_group_field_collection_is_built_from_database_array(): void
+    {
+        $config = new ExtensionConfiguration();
+        $builder = new BeGroupFieldCollectionBuilder($config);
+        $dbValues = [
+            'title' => 'be group',
+            'non_exclude_fields' => 'pages:media,pages:hidden,tt_content:pages,tt_content:date',
+            'tables_select' => 'pages,tt_content,tx_news_domain_model_link,tx_news_domain_model_news'
+        ];
+
+        $collection = $builder->buildFromDatabaseValues($dbValues);
+
+        $nonExcludeFields = NonExcludeFields::createFromDBValue('pages:media,pages:hidden,tt_content:pages,tt_content:date');
+        $tablesSelect = TablesSelect::createFromDBValue('pages,tt_content,tx_news_domain_model_link,tx_news_domain_model_news');
+        $expectedCollection = new BeGroupFieldCollection();
+        $expectedCollection->add($nonExcludeFields);
+        $expectedCollection->add($tablesSelect);
+
+        $this->assertEquals($expectedCollection, $collection);
+    }
+}
