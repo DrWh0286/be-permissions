ff11efc92579412a654be9c714c42b5d100c48c5
Sebastian Hofer <sebastian.hofer@pluswerk.ag>
2022-02-01 14:21:51 +0100

    ✨ Add bulk export triggered by database field

---
+++ Classes/UseCase/BulkExportBeGroupsToConfigurationFiles.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pluswerk\BePermissions\UseCase;
+
+use Pluswerk\BePermissions\Configuration\BeGroupConfiguration;
+use Pluswerk\BePermissions\Model\BeGroup;
+use Pluswerk\BePermissions\Repository\BeGroupConfigurationRepositoryInterface;
+use Pluswerk\BePermissions\Repository\BeGroupRepositoryInterface;
+use TYPO3\CMS\Core\Core\Environment;
+
+final class BulkExportBeGroupsToConfigurationFiles
+{
+    private BeGroupRepositoryInterface $beGroupRepository;
+    private BeGroupConfigurationRepositoryInterface $beGroupConfigurationRepository;
+
+    public function __construct(BeGroupRepositoryInterface $beGroupRepository, BeGroupConfigurationRepositoryInterface $beGroupConfigurationRepository)
+    {
+        $this->beGroupRepository = $beGroupRepository;
+        $this->beGroupConfigurationRepository = $beGroupConfigurationRepository;
+    }
+
+    public function exportGroups(): void
+    {
+        $beGroups = $this->beGroupRepository->findAllForBulkExport();
+        $configPath = Environment::getConfigPath();
+
+        foreach ($beGroups as $beGroup) {
+            $configuration = BeGroupConfiguration::createFromBeGroup($beGroup, $configPath);
+            $this->beGroupConfigurationRepository->write($configuration);
+        }
+    }
+}
