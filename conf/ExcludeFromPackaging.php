b80fbfa4026d37ec9de1e680d3a723ae1eeb9bfb
Sebastian Hofer <sebastian.hofer@pluswerk.ag>
2022-07-01 18:24:26 +0200

    🐛 Fix ext_emconf.php and add exclude from packaging

---
+++ conf/ExcludeFromPackaging.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'directories' => [
+        '.github',
+        'git-hooks'
+    ],
+    'files' => [
+        '.gitignore',
+        'grumphp.yaml',
+        'grumphp-ddev.yaml',
+        'phpunit_functional.xml',
+        'phpunit_integration.xml',
+    ]
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * Extension Manager/Repository config file for ext "higher_education_package".
- */
-
 /** @phpstan-ignore-next-line */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Backend Permissions',
@@ -15,12 +11,10 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Sebastian Hofer',
     'author_email' => 'sebastian.hofer@s-hofer.de',
-    'version' => 'v0.6.1-BETA',
+    'version' => '0.6.1',
     'autoload' => [
         'psr-4' => [
             'SebastianHofer\\BePermissions\\' => 'Classes/',
