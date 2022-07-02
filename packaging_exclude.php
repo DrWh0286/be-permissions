6d8bd435979db081b8e642909d038c72d02dffb7
Sebastian Hofer <sebastian.hofer@pluswerk.ag>
2022-07-02 09:11:44 +0200

    🔧 Add packaging_exclude.php for ignore files for TER

---
+++ packaging_exclude.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'directories' => [
+        '.Build',
+        '.ddev',
+        '.git',
+        '.github',
+        'git-hooks',
+        'Tests',
+    ],
+    'files' => [
+        'grumphp.yml',
+        'grumphp-ddev.yml',
+        '.gitattributes',
+        'composer.lock',
+        '.gitignore',
+        'editorconfig',
+        'packaging_exclude.php',
+        'phpunit.php',
+        'phpunit_functional.php',
+        'phpunit_integration.php',
+    ],
+];
